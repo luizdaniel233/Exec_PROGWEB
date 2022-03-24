1ad2e3fe16d8fcce95972541fdf1c541e0dbf831
Luiz Daniel Marinho dos Santos <luiz.santos@conecthus.org.br>
2022-03-24 18:59:54 -0300

    Exec1 Node list File

--- node.js/exec1.js
+++ node.js/exec1.js
@@ -1,10 +1,52 @@
 const express = require('express')
 const app = express()
 
+const fs = require('fs').promises;
+var file = ''
 
-app.use("/",(req,res) => {
-    const ob = {element : ['express','hello.txt','sails.js','tRex.png','texte.txt'] }
-    res.send(ob.element)
-})
+async function listFile(directory){
+    let arquivos = [];
 
-app.listen(3000,() => console.log("Porta 3000"))
+    let listFile = await fs.readdir(directory);
+
+    for(let k in listFile) {
+        
+        let stat = await fs.stat(directory + '/' + listFile[k]);
+        if(stat.isDirectory())
+            await listFile(directory + '/' + listFile[k], arquivos);
+        else
+            arquivos.push(directory + '/' + listFile[k]);
+
+    }
+
+    return arquivos;
+
+}
+
+async function test(file,res) {
+
+    let arquivos = await listFile(`./${file}`);
+    var cobaia = ''
+    let new_a = []
+
+
+    for(let  i = 0;i < arquivos.length;i++){
+        cobaia = arquivos[i].replace('./arquivos/',"")
+        new_a.push(cobaia);
+    }
+
+    res.send(new_a);
+
+}
+
+process.argv.forEach((val, index) => {
+    file = val;
+});
+
+
+app.use('/',(req,res) => {
+    test(file,res)
+});
+
+app.listen(3000)
+
